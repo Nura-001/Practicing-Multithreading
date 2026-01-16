63b7772b84b5deffce61ee031e3b6d94ce741743
Nurjahan Akter <it23045@mbstu.ac.bd>
2026-01-16 14:27:18 +0600

    Create RegistrarParking.java

---
+++ RegistrarParking.java
@@ -0,0 +1,15 @@
+/*
+ * RegistrarParking represents a car entering the parking system.
+ */
+public class RegistrarParking {
+
+    private int carId;
+
+    public RegistrarParking(int carId) {
+        this.carId = carId;
+    }
+
+    public int getCarId() {
+        return carId;
+    }
+}
